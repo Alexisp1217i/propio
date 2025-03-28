976ef51c40945609b7e60e56f2d5256e90ff1491
Jesus Alexis Jimenez Aldaba <aldabaalexis85@gmail.com>
2025-03-27 18:04:52 -0600

    Add files via upload

---
+++ eva2_16_Composicion/src/main/java/com/mycompany/eva2_16_composicion/Eva2_16_Composicion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_composicion;
+
+/**
+ *
+ * @author nancy
+ */
+public class Eva2_16_Composicion {
+
+    public static void main(String[] args) {
+        Cliente client =new Cliente("alexis", "Aldaba", 18, "papaya", "turepera", 2154151);
+        System.out.println(client);
+    }
+}
